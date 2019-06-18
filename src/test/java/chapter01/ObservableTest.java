7b049519b1be8884c22ace9b26e2edc2034e1259
Valentine Shemyako <v.shemyako@gmail.com>
2019-06-18 21:59:01 +0300

    Basic usage of synchronous Observable

---
+++ src/test/java/chapter01/ObservableTest.java
@@ -0,0 +1,21 @@
+package chapter01;
+
+import org.junit.Test;
+import rx.Observable;
+import rx.Subscriber;
+
+public class ObservableTest {
+
+    @Test
+    public void shouldBeSynchronousByDefault() {
+        Observable<String> wordsObserved = Observable.create((Subscriber<? super String> s) -> {
+            s.onNext("first");
+            s.onNext("second");
+            s.onNext("third");
+            s.onCompleted();
+        });
+        wordsObserved
+                .map((String word) -> String.format("%s has been processed", word))
+                .subscribe(System.out::println);
+    }
+}
