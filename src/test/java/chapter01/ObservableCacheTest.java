010d48f1dae0314abf50602425dcba2bdb281b6d
Valentine Shemyako <v.shemyako@gmail.com>
2019-06-22 12:29:37 +0300

    Verified functionality of the cache method

---
+++ src/test/java/chapter01/ObservableCacheTest.java
@@ -0,0 +1,30 @@
+package chapter01;
+
+import org.junit.Assert;
+import org.junit.Test;
+import rx.Observable;
+import rx.Subscriber;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Verifies functionality of {@link Observable#cache()} method.
+ */
+public class ObservableCacheTest {
+
+    @Test
+    public void shouldCacheEvents() {
+        // Arrange
+        AtomicInteger invocations = new AtomicInteger();
+        Observable<String> cachedObservable = Observable.create((Subscriber<? super String> subscriber) -> {
+            invocations.getAndIncrement();
+            subscriber.onNext("Pushed");
+            subscriber.onCompleted();
+        }).cache();
+        // Act
+        cachedObservable.subscribe(System.out::println);
+        cachedObservable.subscribe(System.out::println);
+        // Assert
+        Assert.assertEquals(1, invocations.get());
+    }
+}
