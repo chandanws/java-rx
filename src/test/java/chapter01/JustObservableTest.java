04b668e836064156159d424212476524183156ba
Valentine Shemyako <v.shemyako@gmail.com>
2019-06-22 11:08:36 +0300

    Verified functionality of Observable.just method

---
+++ src/test/java/chapter01/JustObservableTest.java
@@ -0,0 +1,32 @@
+package chapter01;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import rx.Observable;
+import rx.Subscriber;
+
+/**
+ * Tests {@link rx.Observable} which emits only single event and gracefully stops.
+ */
+public class JustObservableTest {
+
+    private Subscriber<Integer> subscriber;
+
+    @Before
+    @SuppressWarnings("unchecked")
+    public void setUp() {
+        subscriber = Mockito.spy(Subscriber.class);
+    }
+
+    @Test
+    public void shouldEmitSingleEvent() {
+        // Arrange
+        Observable<Integer> justObservable = Observable.just(1);
+        // Act
+        justObservable.subscribe(subscriber);
+        // Assert
+        Mockito.verify(subscriber).onNext(Mockito.any());
+        Mockito.verify(subscriber).onCompleted();
+    }
+}
