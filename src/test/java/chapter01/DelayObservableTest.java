697b8f8cf477df839520e08f3bd1efac869fea6c
Valentine Shemyako <v.shemyako@gmail.com>
2019-06-23 15:46:15 +0300

    Investigated concurrent functionality of flatMap operator

---
+++ src/test/java/chapter01/DelayObservableTest.java
@@ -0,0 +1,33 @@
+package chapter01;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import rx.Observable;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Verifies behavior of the {@link rx.Observable#delay(long, TimeUnit) delay}
+ * and {@link rx.Observable#timer(long, TimeUnit) timer} methods.
+ */
+@Slf4j
+public class DelayObservableTest {
+
+    @Test
+    public void shouldPostponeEvents() throws InterruptedException {
+        Observable.just(1, 2, 3)
+                .delay(1, TimeUnit.SECONDS)
+                .subscribe(event -> log.debug("{}", event));
+
+        TimeUnit.SECONDS.sleep(2);
+    }
+
+    @Test
+    public void shouldDelayEvents() throws InterruptedException {
+        Observable.just("Lorem", "ipsum", "dolor", "sit", "amet", "consectetur", "adipiscing", "elit")
+                .flatMap(word -> Observable.timer(word.length(), TimeUnit.SECONDS).map(x -> word))
+                .subscribe(event -> log.debug("{}", event));
+
+        TimeUnit.SECONDS.sleep(10);
+    }
+}
