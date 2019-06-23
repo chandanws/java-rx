0d08859e0d2db5b7845799edff2602354f15f121
Valentine Shemyako <v.shemyako@gmail.com>
2019-06-23 09:12:18 +0300

    Verified functionality of map method

---
+++ src/test/java/chapter01/ObservableMapTest.java
@@ -0,0 +1,21 @@
+package chapter01;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import rx.Observable;
+import rx.functions.Func1;
+
+/**
+ * Verifies functionality offered by {@link Observable#map(Func1) map} function.
+ */
+@Slf4j
+public class ObservableMapTest {
+
+    @Test
+    public void shouldConvertEvents() {
+        Observable
+                .just(1, 2)
+                .map(number -> number * 2)
+                .subscribe(event -> log.debug("{}", event));
+    }
+}
