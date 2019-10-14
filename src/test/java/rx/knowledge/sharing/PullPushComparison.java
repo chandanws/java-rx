cb2236161fc69bcd0d26814739f11b2195faa5cc
Valentine Shemyako <v.shemyako@gmail.com>
2019-10-14 22:00:14 +0300

    Created knowledge-sharing package.

---
+++ src/test/java/rx/knowledge/sharing/PullPushComparison.java
@@ -0,0 +1,19 @@
+package rx.knowledge.sharing;
+
+/**
+ * Depicts difference between push and pull approaches in writing code.
+ */
+public class PullPushComparison {
+
+    private static class Stove {
+
+    }
+
+    private static class NonWhistlingKettle {
+
+    }
+
+    private static class WhistlingKettle {
+
+    }
+}
---
+++ src/test/java/rx/knowledge/sharing/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * <h3>RxJava (Reactive Java) - A library for composing asynchronous and event-based programs.</h3>
+ * <br>
+ * Reactive means that we <b>react</b> to some changes, whenever those changes happen, rather than trying to <b>request
+ * and wait</b> until when those changes actually happen.
+ * <br>
+ */
+package rx.knowledge.sharing;
