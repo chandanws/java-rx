0ff04605c80ab30b0dab5bea4427b09e84feaa28
Valentine Shemyako <v.shemyako@gmail.com>
2019-10-16 20:25:54 +0300

    Created Notes class to list main ideas from the RxJava book.

---
+++ src/test/java/rx/knowledge/sharing/Notes.java
@@ -0,0 +1,13 @@
+package rx.knowledge.sharing;
+
+/**
+ * <h3>RxJava Notes.</h3>
+ * <ol>
+ * <li>An extension of <b>Observable</b> design pattern. Main idea to support events pushing.
+ * RxJava allows to combine those events asynchronously.</li>
+ * <li>Also supports feedback channel, so called asynchronous pulling (used to implement back-pressure mechanism).</li>
+ * <li>Central type - {@link rx.Observable}. Represents stream of events.</li>
+ * </ol>
+ */
+public class Notes {
+}
