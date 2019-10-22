c2ccf1187633b1aee2dc42d99c9e50b89f8b47cd
Valentine Shemyako <v.shemyako@gmail.com>
2019-10-22 20:23:46 +0300

    Examined different cases of errors being thrown in subscriber's callbacks

--- src/test/java/rx/knowledge/sharing/error/ErrorNote001_OnErrorCallback.java
+++ src/test/java/rx/knowledge/sharing/error/ErrorNote001_OnErrorCallback.java
@@ -4,11 +4,11 @@ import org.junit.Test;
 import rx.Observable;
 
 import static rx.knowledge.sharing.error.Messages.ERROR;
+import static rx.knowledge.sharing.error.Messages.EVENT;
 import static rx.knowledge.sharing.error.Messages.FINISH_MESSAGE;
 import static rx.knowledge.sharing.error.Messages.START_MESSAGE;
 
 public class ErrorNote001_OnErrorCallback {
-
 
     /**
      * Subscriber can provide a callback which will be invoked in case Observable's
@@ -23,6 +23,44 @@ public class ErrorNote001_OnErrorCallback {
                         event -> System.out.println(event),
                         error -> System.err.println(error)
                 );
+
+        System.out.println(FINISH_MESSAGE);
+    }
+
+    /**
+     * In case subscriber's error-handling callback itself throws an error ->
+     * thread is interrupted.
+     */
+    @Test
+    public void shouldNotifyObserverAboutError_2() {
+        System.out.println(START_MESSAGE);
+
+        Observable.<String>error(new IllegalStateException(ERROR))
+                .subscribe(
+                        event -> System.out.println(event),
+                        error -> {
+                            throw new RuntimeException();
+                        }
+                );
+
+        System.out.println(FINISH_MESSAGE);
+    }
+
+    /**
+     * In case subscriber's onNext-handling callback itself throws an error ->
+     * subscriber's onError-handling callback is invoked.
+     */
+    @Test
+    public void shouldNotifyObserverAboutError_3() {
+        System.out.println(START_MESSAGE);
+
+        Observable.just(EVENT)
+                  .subscribe(
+                          event -> {
+                              throw new IllegalStateException();
+                          },
+                          error -> System.err.println(error)
+                  );
 
         System.out.println(FINISH_MESSAGE);
     }
--- src/test/java/rx/knowledge/sharing/error/Messages.java
+++ src/test/java/rx/knowledge/sharing/error/Messages.java
@@ -2,6 +2,7 @@ package rx.knowledge.sharing.error;
 
 public class Messages {
     static final String START_MESSAGE = "STARTED!";
+    static final String EVENT = "EVENT!";
     static final String ERROR = "BOOM!";
     static final String FINISH_MESSAGE = "FINISHED!";
 }
